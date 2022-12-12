5c76e6824ee0a8b30cfd95a5c45926956a2e5f85
Poera <l2321230429@gmail.com>
2022-12-12 21:09:55 +0800

    完成基本功能

--- eivelia_bot/plugins/nonebot_plugin_fire/__init__.py
+++ eivelia_bot/plugins/nonebot_plugin_fire/__init__.py
@@ -1,8 +1,10 @@
-import asyncio,json
-from nonebot import require,get_driver,get_bot
+import asyncio
+import json
+from nonebot import require, get_driver, get_bot
 from nonebot.log import logger
 from nonebot.plugin import PluginMetadata
 
+from nonebot.adapters.onebot.v11 import Bot
 from eivelia_bot.plugins.mqtt.mqtt import get_client
 
 driver = get_driver()
@@ -16,12 +18,20 @@ __plugin_meta__ = PluginMetadata(
 )
 
 
-def on_message(client, topic, payload, qos, properties):
+async def on_message(client, topic, payload, qos, properties):
+    if json.loads(payload)['state'] == "fire":
+        try:
+            bot = get_bot()
+            if isinstance(bot, Bot):
+                await bot.send_private_msg(user_id=3539471990, message="fire!warning!")
+        except KeyError:
+            logger.info('no bot')
     logger.info(f'{topic} RECV MSG:'+str(json.loads(payload)))
+
 
 @driver.on_startup
 async def live():
     await asyncio.sleep(3)
     client = get_client(__plugin_meta__.name)
-    client.subscribe('poera_out',qos=1)
+    client.subscribe('poera_out', qos=1)
     client.on_message = on_message
